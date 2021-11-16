b9a7c466f49488d2378a9b9304ec58dbf5550e21
Oleksii Kononenko <canon318i@gmail.com>
2021-11-16 17:49:26 +0200

    Added closing after link
    1. Now  menu is closed when any menu link is pressed
    2. Now scroll is locked when meniu is opened.

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -2,14 +2,16 @@
   const mobileMenu = document.querySelector('[data-mobilemenu]');
   const mobileMenuClose = document.querySelector('[data-mobilemenu-close]');
   const mobileMenuOpen = document.querySelector('[data-mobilemenu-open]');
+  const mobileMenuList = document.querySelector('[data-mobilemenu-list]');
  
 
   const toggleMenu = () => {
     const isMenuOpen =
-      mobileMenuClose.getAttribute('aria-expanded') === 'true' || false;
-    mobileMenuClose.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenuOpen.getAttribute('aria-expanded') === 'true' || false;
+    mobileMenuOpen.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenuClose.setAttribute('aria-expanded', isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-    mobileMenuOpen.classList.toggle('visually-hidden');
+    mobileMenuOpen.classList.toggle('visually-hidden'); //прячем гамбургер при открывании меню
  
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
@@ -17,17 +19,19 @@
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
-  mobileMenuClose.addEventListener('click', toggleMenu);
-  mobileMenuOpen.addEventListener('click', toggleMenu);
+  mobileMenuClose.addEventListener('click', toggleMenu);   
+  mobileMenuOpen.addEventListener('click', toggleMenu); 
+  mobileMenuList.addEventListener('click', toggleMenu); //закрываем меню при выборе ссылки
 
-
+//СНЯТЬ КОММЕНТАРИЙ ПОСЛЕ РЕАЛИЗАЦИИ ДЕСКТОПНОГО МЕНЮ
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
   // window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
   //   if (!e.matches) return;
   //   mobileMenu.classList.remove('is-open');
-  //   mobileMenuClose.classList.remove('is-open');
-  //   openMenuBtn.setAttribute('aria-expanded', false);
+  //   mobileMenuOpen.classList.remove('visually-hidden');
+  //   mobileMenuOpen.setAttribute('aria-expanded', false);
+  //   mobileMenuClose.setAttribute('aria-expanded', false);
   //   bodyScrollLock.enableBodyScroll(document.body);
   // });
 })();
